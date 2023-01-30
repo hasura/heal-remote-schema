78c3c25f07d85472d9811d16eda2fa6453d8cc4c
Paritosh Raj <paritosh@hasura.io>
2023-01-30 13:26:33 +0530

    init

---
+++ README.md
@@ -0,0 +1,53 @@
+## Heal Remote Schemas
+
+A script for auto healing the inconsistent remote schemas in graphql-engine.
+
+### Configurable parameters:
+
+This script uses the following configurable parameters (environment variables) for running the auto-healing node server:
+1. `GRAPHQL_ENGINE_URL`: The url of the graphql engine (e.g. `http://localhost:8080`)
+2. `HASURA_GRAPHQL_ADMIN_SECRET`: The admin secret of the graphql engine.
+3. `CHECK_INTERVAL_SECONDS`: The interval for checking the metadata for any inconsistencies (in seconds).
+4. `RELOAD_INTERVAL_SECONDS`: The interval for reloading the remote schemas (in seconds).
+
+### How does this work?
+
+The checking thread runs the following query to the graphql-engine every `CHECK_INTERVAL_SECONDS`:
+```
+POST v1/metadata
+admin
+
+{
+  "type": "get_inconsistent_metadata",
+  "args": {}
+}
+```
+If the above query returns any inconsistent remote schema, it stores them in an array.
+
+Now, the reloading thread checks if the inconsistent remote schema array is empty every `RELOAD_INTERVAL_SECONDS`. If it
+is empty, then it will just log:
+```
+All remote schemas are consistent.
+```
+
+Now, if the array is not empty, then it will try reloading the inconsistent remote schemas:
+```
+POST v1/metadata
+admin
+
+{
+  "type" : "reload_metadata",
+  "args": {
+    "reload_remote_schemas": *inconsistentSchemas
+  }
+}
+```
+
+To run this server locally (via node):
+```
+npm install
+touch .env
+npm start
+```
+
+
---
+++ server.js
@@ -0,0 +1,98 @@
+'use strict';
+
+const axios = require("axios");
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+  
+}
+
+const gqlEngine = process.env.GRAPHQL_ENGINE_URL;
+const checkInterval = process.env.CHECK_INTERVAL_SECONDS;
+const reloadInterval = process.env.RELOAD_INTERVAL_SECONDS;
+const adminSecret = process.env.HASURA_GRAPHQL_ADMIN_SECRET;
+
+var inconsistentSchemas = []
+
+// Check if the graphql engine var is set
+if (! gqlEngine) {
+  throw new Error('expected a non null environment variable GRAPHQL_ENGINE_URL');
+}
+const gqlEngineURL = new URL(gqlEngine);
+console.log(`GQL engine running on ${gqlEngineURL.href}`);
+
+const headers = {
+  'x-hasura-admin-secret': adminSecret
+}
+
+// Async function to check the remote schemas
+async function runCheck() {
+  gqlEngineURL.pathname = 'v1/metadata'
+  axios.post(gqlEngineURL.href, {
+    "type": "get_inconsistent_metadata",
+    "args": {}
+  }, {headers: headers})
+  .then(function (response) {
+    let isConsistent = response.data["is_consistent"];
+    if (!isConsistent) {
+      inconsistentSchemas = getAllInconsistentRemoteSchemas(response.data['inconsistent_objects']);
+    }
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+} 
+
+function runCheckEveryNSeconds(n) {
+  setInterval(runCheck, n * 1000);
+}
+// Run check every N seconds
+runCheckEveryNSeconds(checkInterval);
+
+// Async function to reload the inconsistent remote schemas
+async function runReload() {
+  if (inconsistentSchemas.length == 0) {
+    console.log("All remote schemas are consistent.")
+  }
+  else {
+    console.log(inconsistentSchemas);
+    gqlEngineURL.pathname = 'v1/metadata'
+    axios.post(gqlEngineURL.href, {
+      "type" : "reload_metadata",
+      "args": {
+        "reload_remote_schemas": inconsistentSchemas
+      }
+    }, {headers: headers})
+    .then(function (response) {
+      let isConsistent = response.data["is_consistent"];
+      if (!isConsistent) {
+        inconsistentSchemas = getAllInconsistentRemoteSchemas(response.data['inconsistent_objects']);
+      }
+      else {
+        inconsistentSchemas = [];
+      }
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }
+} 
+
+function runReloadEveryNSeconds(n) {
+  setInterval(runReload, n * 1000);
+}
+
+// Run reload every N seconds
+runReloadEveryNSeconds(reloadInterval);
+
+// ----------------------- Utils -----------------------
+
+function getAllInconsistentRemoteSchemas(jsonData) {
+  var inconsistentRemoteSchemas = []
+  for (let obj of jsonData) {
+    if (obj["name"].startsWith("remote_schema")) {
+      const remoteSchemaName = obj["name"].split(' ')[1];
+      inconsistentRemoteSchemas.push(remoteSchemaName);
+    }
+  }
+  return inconsistentRemoteSchemas
+}
